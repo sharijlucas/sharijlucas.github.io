ac5d88cfb7fdd5c97c8d154328673e9d4313d1d8
sharijlucas <luc19004@byui.edu>
2020-02-03 15:35:44 -0700

    update

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,4 +1,6 @@
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
 function toggleMenu() {
-    
-    document.getElementById("primaryNav").classList.toggle("hide");
+  document.querySelector(".navigation").classList.toggle("responsive");
 }
