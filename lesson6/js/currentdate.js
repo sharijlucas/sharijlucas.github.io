cc9dce9936af004ab806db1eb6fca9fe8bb11b19
sharijlucas <luc19004@byui.edu>
2020-02-27 10:16:22 -0700

    update

--- lesson6/js/currentdate.js
+++ lesson6/js/currentdate.js
@@ -2,4 +2,6 @@ var now = new Date();
 
 var Weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 var Month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-document.write(Weekday[now.getDay()] + ", " + Month[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear());
+var date = (Weekday[now.getDay()] + ", " + Month[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear());
+
+document.getElementById("date").innerHTML = date;
--- lesson7/js/messages.js
+++ lesson7/js/messages.js
@@ -1,5 +1,6 @@
 var today = new Date();
 var date = today.getDay();
+var msg = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
 if (date == 5){
-    document.write("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.")
+    document.getElementById("msg").textContent = msg;
 }
