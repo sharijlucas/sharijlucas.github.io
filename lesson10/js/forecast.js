f56e82381d3bfc0eb796f0d581debcc972b34952
sharijlucas <luc19004@byui.edu>
2020-03-20 19:58:45 -0600

    update

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,4 +1,4 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=50b16ae8477ff34ac4b8bd2d7a08dc0a';
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=50b16ae8477ff34ac4b8bd2d7a08dc0a';
 
 fetch(apiURL)
   .then((response) => response.json())
